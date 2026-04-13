e4061af6ce60e71dd444325f43ed53a256a90fda
michelarellano <arellano_jimenez@hotmail.com>
2026-04-13 00:37:36 -0600

    feat: estructura inicial del proyecto Spring Boot

---
+++ src/main/java/hcltech/HcltechApplication.java
@@ -0,0 +1,13 @@
+package hcltech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HcltechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HcltechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hcltech/HcltechApplicationTests.java
@@ -0,0 +1,13 @@
+package hcltech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HcltechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
